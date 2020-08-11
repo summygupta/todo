0a05d35eaa3ba4a855ec44fdde41f275e0e8480e
Summy Gupta <51532653+summygupta@users.noreply.github.com>
2020-08-12 01:37:29 +0530

    todo

---
+++ django-todo-react/backend/todo/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Todo # add this
+
+class TodoAdmin(admin.ModelAdmin):  # add this
+    list_display = ('title', 'description', 'completed') # add this
+
+admin.site.register(Todo, TodoAdmin)
